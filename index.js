3d974bae6169c11606bdb89f34ba516d67167635
Mia-v <vasilyeva.mariya@gmail.com>
2020-08-04 22:42:48 +0200

    Made a simple arithmetical functions

---
+++ index.js
@@ -0,0 +1,9 @@
+const arithm = {
+    getSum: function(num1, num2 ){ return num1 + num2;},
+    getDifference: function(num1, num2) { return num1 - num2;},
+    getProduct: function(num1, num2) { return num1 * num2;},
+    getQuotient: function(num1, num2) { return num1 / num2;},
+}
+
+
+module.exports = arithm;
